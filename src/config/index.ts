923e3f44a8e0750a1ef98bf5e3390de20305b88f
Kotov <kotov.smtp@gmail.com>
2021-12-22 20:02:03 +0200

    ⚙️ config updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,17 @@
 import dotenv from "dotenv";
 
-// load env file
-dotenv.config({
-  path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env",
-});
+dotenv.config();
 
 export const NODE_ENV = process.env.NODE_ENV || "development";
-export const HTTP_PORT = process.env.HTTP_PORT;
-export const HTTPS_PORT = process.env.HTTPS_PORT;
+export const PORT = process.env.PORT || 5000;
 export const DB_URL = process.env.DB_URL;
+export const JWT_SECRET = process.env.JWT_SECRET || "";
+export const APP_JWT_SECRET = process.env.APP_JWT_SECRET || "";
 
-export default { NODE_ENV, HTTP_PORT, HTTPS_PORT, DB_URL };
+export default {
+  NODE_ENV,
+  PORT,
+  DB_URL,
+  JWT_SECRET,
+  APP_JWT_SECRET,
+};
