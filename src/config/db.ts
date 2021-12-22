8614ecf2a1f9155e286f542c7c3cc375aa4721de
Kotov <kotov.smtp@gmail.com>
2021-12-22 20:10:27 +0200

    ⚙️ config updated

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,13 @@
 import path from "path";
 import { createConnection } from "typeorm";
-import { DB_URL } from ".";
+import { DATABASE_URL } from ".";
 
 import { User } from "../entities/User";
 
 export const connectDB = async () => {
   const conn = await createConnection({
     type: "postgres",
-    url: DB_URL,
+    url: DATABASE_URL,
     logging: true,
     synchronize: true,
     migrations: [path.join(__dirname, "./migrations/*")],
--- src/config/index.ts
+++ src/config/index.ts
@@ -4,14 +4,14 @@ dotenv.config();
 
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT || 5000;
-export const DB_URL = process.env.DB_URL;
+export const DATABASE_URL = process.env.DATABASE_URL;
 export const JWT_SECRET = process.env.JWT_SECRET || "";
 export const APP_JWT_SECRET = process.env.APP_JWT_SECRET || "";
 
 export default {
   NODE_ENV,
   PORT,
-  DB_URL,
+  DATABASE_URL,
   JWT_SECRET,
   APP_JWT_SECRET,
 };
