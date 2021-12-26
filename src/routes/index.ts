aff606431b96c79d7a94d84c3036daf51683f7bb
Kotov <kotov.smtp@gmail.com>
2021-12-26 03:24:19 +0200

    ⚛️ routes added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,19 +7,39 @@ import authController from "./auth/ctrl";
 
 // Middlewares
 import { auth } from "../middlewares/auth.middleware";
+import { appAuth } from "../middlewares/appAuth.middleware";
 
 const router = Router();
 
 router.get("/ping", (_, res) => res.send("pong"));
 
 // auth
-router.route("/register").post(authController.register);
-router.route("/login").post(authController.login);
+router.route("/auth/register").post(authController.register);
+router.route("/auth/login").post(authController.login);
+
+router.route("/get-app-token").get(authController.getAppToken);
 
 // user
 router.route("/user/me").get(auth, userController.me);
 
+router.route("/user/:id/verify-email").post(userController.verifyEmail);
+
+router.route("/user/reset-password").get(auth, userController.resetPassword);
+
+router
+  .route("/user")
+  .put([auth, appAuth], userController.updateUserScore)
+  .delete(auth, userController.deleteUser);
+
 // leaderboard
-router.route("/leaderboard").get(leaderboardController.getLeaderboard);
+router
+  .route("/leaderboard")
+  .get(auth, leaderboardController.getLeaderboard)
+  .delete(appAuth, leaderboardController.clearLeaderboard);
+
+router
+  .route("/leaderboard/best")
+  .get(leaderboardController.getLeaders)
+  .delete(appAuth, leaderboardController.clearLeaders);
 
 export default router;
